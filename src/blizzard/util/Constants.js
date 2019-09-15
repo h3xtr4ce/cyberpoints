a0294694302608fb614acb0b481c24b3a0ee02c7
Yankı <kendinikertenkelebek@me.com>
2019-09-15 11:01:41 +0300

    added api roots

--- src/blizzard/util/Constants.js
+++ src/blizzard/util/Constants.js
@@ -9,6 +9,45 @@ exports.Defaults = {
   debug: false,
 };
 
+exports.Roots = [
+  {
+    region: 'us',
+    hostname: 'https://us.api.blizzard.com',
+    def: 'en_US',
+    locales: ['en_US', 'es_MX', 'pt_BR'],
+  },
+  {
+    region: 'eu',
+    hostname: 'https://eu.api.blizzard.com',
+    def: 'en_GB',
+    locales: ['en_GB', 'es_ES', 'fr_FR', 'ru_RU', 'de_DE', 'pt_PT', 'it_IT'],
+  },
+  {
+    region: 'sea',
+    hostname: 'https://sea.api.blizzard.com',
+    def: 'en_US',
+    locales: ['en_US'],
+  },
+  {
+    region: 'kr',
+    hostname: 'https://kr.api.blizzard.com',
+    def: 'ko_KR',
+    locales: ['ko_KR', 'en_GB', 'en_US'],
+  },
+  {
+    region: 'tw',
+    hostname: 'https://tw.api.blizzard.com',
+    def: 'zh_TW',
+    locales: ['zh_TW', 'en_GB', 'en_US'],
+  },
+  {
+    region: 'cn',
+    hostname: 'https://gateway.battlenet.com.cn',
+    def: 'zh_CN',
+    locales: ['zh_CN', 'en_GB', 'en_US'],
+  },
+];
+
 exports.ErrorTexts = {
   noKey: 'Options must be have \'key\' property!',
   noSecret: 'Options must be have \'secret\' property!',
